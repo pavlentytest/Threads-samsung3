65c4164b0e55edeb5d4792919a8ace2a2e072d5c
Pvl <impetus@yandex.ru>
2019-12-21 17:12:01 +0300

    add: - Синхронизация по объекту - wait() + notift() - Остановка потока

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // процесс (программа)
         // поток
         // 1. extends Thread => run();
@@ -16,17 +16,27 @@ public class Main {
         t1.start();
         MyThread t2 = new MyThread("-");
         t2.start();
+        Thread.sleep(3000);
+        t1.flag = false;
+        test("1 stopped!");
+        t1.join(); // ждет завершение потока - требуется время
+        test("Fact 1 stopped!");
+
 
    // [-][+][-][+]
     }
-
-    public synchronized static void test(String m) {
+    private static Object key = new Object();
+    public static void test(String m) {
         try {
-            System.out.print("[");
-            Thread.sleep(1000);
-            System.out.print(m);
-            Thread.sleep(1000);
-            System.out.print("]");
+            synchronized (key) {
+                System.out.print("[");
+                Thread.sleep(1000);
+                System.out.print(m);
+                Thread.sleep(1000);
+                System.out.print("]");
+             //   key.notify(); // возобновить поток кот. в режиме wait()
+             //   key.wait(); // поставить поток в режим wait до вызова notify()
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/com/company/MyThread.java
+++ src/com/company/MyThread.java
@@ -2,13 +2,14 @@ package com.company;
 
 public class MyThread extends Thread {
     private String message;
+    public boolean flag = true;
     MyThread(String m) {
         this.message = m;
     }
 
     @Override
     public void run() {
-        while(true) {
+        while(flag) {
             Main.test(message);
         }
     }
